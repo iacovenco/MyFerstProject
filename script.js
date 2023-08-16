e20f9cf0a9ba1258c3e40313eb0dfb4f5074d6cf
totsky.ia <valihmanov777@gmail.com>
2023-08-16 18:27:11 +0300

    Добавлены переменные и вывод на стр.

---
+++ script.js
@@ -0,0 +1,12 @@
+const Hello = "Hello";
+
+const title = 0;
+const screens = 1;
+const screenPrice = 2;
+const rollback = 3;
+const fullPrice = 4;
+const adaptive = 5;
+
+console.log(Hello);
+
+alert("Приветствую");

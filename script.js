faf02cb980044947a3c4be290c1aa331e53c43e6
totsky.ia <valihmanov777@gmail.com>
2023-08-18 15:13:32 +0300

    Добавил вторую часть задания 2го урока

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ let screenPrice = 1312;
 let rollback = 99;
 let fullPrice = 90;
 let adaptive = true;
+
+console.log(title, fullPrice, adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+/* 
+console.log(screens.toLowerCase());
+console.log(screens.split(","));     
+ Не совсем понял нужно и разбить в масив,
+ и привести к нижнему регистру в одном выводе или в отдельных. 
+*/
+console.log(screens.toLowerCase().split(","));
+console.log(fullPrice * (rollback / 100)); //хочу быть посредником)

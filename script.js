e2fafa5c4b4d5c5b2ed2a193d5ac0fae8618a265
totsky.ia <valihmanov777@gmail.com>
2023-08-18 22:34:21 +0300

    исправил объявления переменных с let на const

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let title = "lesson02";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 1312;
-let rollback = 99;
-let fullPrice = 90;
-let adaptive = true;
+const title = "lesson02";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 1312;
+const rollback = 99;
+const fullPrice = 90;
+const adaptive = true;
 
 console.log(title, fullPrice, adaptive);
 console.log(screens.length);

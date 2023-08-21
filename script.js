a00d2bf9c2e7a1bc3233ac99f23401e0f4474060
totsky.ia <valihmanov777@gmail.com>
2023-08-21 20:45:20 +0300

    Исправил let на const + удалил коментарии

--- script.js
+++ script.js
@@ -1,60 +1,41 @@
 "use strict";
 
-let title = prompt("Как называется ваш проект?");
+const title = prompt("Как называется ваш проект?");
 console.log(title);
 
-let screens = prompt("Какие типы экранов нужно разработать?");
+const screens = prompt("Какие типы экранов нужно разработать?");
 console.log(screens);
 
-let screensPrice = +prompt("Сколько будет стоить данная работа?");
+const screensPrice = +prompt("Сколько будет стоить данная работа?");
 console.log(screensPrice);
 
 let adaptive = prompt("Нужен ли адаптив на сайте?");
+
 if (adaptive) {
   adaptive = console.log(Boolean(" "));
 } else {
   adaptive = console.log(Boolean(""));
 }
 
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
 console.log(service1);
-let servicePrice1 = +prompt("Сколько это будет стоить?");
+
+const servicePrice1 = +prompt("Сколько это будет стоить?");
 console.log(servicePrice1);
 
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
 console.log(service2);
-let servicePrice2 = +prompt("Сколько это будет стоить?");
+
+const servicePrice2 = +prompt("Сколько это будет стоить?");
 console.log(servicePrice2);
 
-let fullPrice = screensPrice + servicePrice1 + servicePrice2;
+const fullPrice = screensPrice + servicePrice1 + servicePrice2;
 console.log(fullPrice);
-/*
-const kickback = "25%";
-let servicePercentPrice = fullPrice;
-servicePercentPrice =
-  servicePercentPrice - (servicePercentPrice / 100) * parseFloat(kickback);
-console.log(servicePercentPrice);
-Тут у меня именно вычитание  процентов,но я так понял нужно
- откат в 'рублях' вводить
-*/
 
-let rebate = 5000;
-let servicePercentPrice = Math.ceil(fullPrice - rebate);
+const rebate = 5000;
+const servicePercentPrice = Math.ceil(fullPrice - rebate);
 
 console.log(servicePercentPrice);
-/*
-if (fullPrice > 29999) {
-  console.log("Даем скидку в 10%");
-} else if (fullPrice > 14999 && fullPrice <= 29999) {
-  console.log("Даем скидку в 5%");
-} else if (fullPrice > 0 && fullPrice <= 14999) {
-  console.log("Скидка не предусмотрена");
-} else {
-  console.log("Что то пошло не так");
-}
-При отрицательном значении выводит "Скидка не предусмотрена",
-а не "Что то пошло не так".
-*/
 
 switch (true) {
   case fullPrice > 29999:
@@ -69,5 +50,3 @@ switch (true) {
   default:
     console.log("Что то пошло не так");
 }
-
-alert("Подтвердить действие");

0bb81fdd70c78d62a6068dd24afda6e911a5d7b2
totsky.ia <valihmanov777@gmail.com>
2023-08-24 14:16:02 +0300

    сделал код более читабельным

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
 "use strict";
 
 const title = prompt("Как называется ваш проект?");
-console.log(title);
 
 const screens = prompt("Какие типы экранов нужно разработать?");
-console.log(screens);
 
 const screensPrice = +prompt("Сколько будет стоить данная работа?");
-console.log(screensPrice);
 
 let adaptive = prompt("Нужен ли адаптив на сайте?");
 
-if (adaptive) {
-  adaptive = console.log(Boolean(" "));
-} else {
-  adaptive = console.log(Boolean(""));
-}
-
 const service1 = prompt("Какой дополнительный тип услуги нужен?");
-console.log(service1);
 
 const servicePrice1 = +prompt("Сколько это будет стоить?");
-console.log(servicePrice1);
 
 const service2 = prompt("Какой дополнительный тип услуги нужен?");
-console.log(service2);
 
 const servicePrice2 = +prompt("Сколько это будет стоить?");
-console.log(servicePrice2);
 
 const fullPrice = screensPrice + servicePrice1 + servicePrice2;
-console.log(fullPrice);
 
 const rebate = 5000;
 const servicePercentPrice = Math.ceil(fullPrice - rebate);
-
-console.log(servicePercentPrice);
 
 switch (true) {
   case fullPrice > 29999:
@@ -50,3 +34,20 @@ switch (true) {
   default:
     console.log("Что то пошло не так");
 }
+
+console.log(title);
+console.log(screens);
+console.log(screensPrice);
+
+if (adaptive) {
+  adaptive = console.log(Boolean(" "));
+} else {
+  adaptive = console.log(Boolean(""));
+}
+
+console.log(service1);
+console.log(servicePrice1);
+console.log(service2);
+console.log(servicePrice2);
+console.log(fullPrice);
+console.log(servicePercentPrice);
